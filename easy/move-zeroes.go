3e6f0911f37a211dcfa24580f84d1fc063c125da
avinash92c <avinash92c@gmail.com>
2020-04-03 17:02:47 +0530

    Create move-zeroes.go

---
+++ easy/move-zeroes.go
@@ -0,0 +1,13 @@
+func moveZeroes(nums []int) {
+	count := 0
+	for i :=0;i<len(nums);i++ {
+		if nums[i] != 0 {
+			nums[count] = nums[i]
+			count++
+		}
+	}
+
+	for i := count; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}

2353c51338eb0f527a230329d3818c19c090236c
avinash92c <avinash92c@gmail.com>
2020-04-03 17:05:51 +0530

    Create valid-anagram.go

---
+++ easy/valid-anagram.go
@@ -0,0 +1,25 @@
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	count := make([]int, 26)
+
+	for index, _ := range count {
+		count[index] = 0
+	}
+
+	for i := 0; i < len(s); i++ {
+		count[int(s[i])-int('a')]++
+		count[int(t[i])-int('a')]--
+	}
+
+	fmt.Println(count)
+
+	for _, val := range count {
+		if val != 0 {
+			return false
+		}
+	}
+	return true
+}

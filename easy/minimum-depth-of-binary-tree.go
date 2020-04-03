0fbb117aad840f5ccc68c71fa8525fc89e54dd89
avinash92c <avinash92c@gmail.com>
2020-04-03 17:16:24 +0530

    Create minimum-depth-of-binary-tree.go

---
+++ easy/minimum-depth-of-binary-tree.go
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return min(minDepth(root.Left), minDepth(root.Right)) + 1
+}
+
+func min(x, y int) int {
+    if x==0 && y!=0{
+        return y
+    }else if x!=0&&y==0{
+        return x
+    }
+	if x < y {
+		return x
+	}
+	return y
+}
